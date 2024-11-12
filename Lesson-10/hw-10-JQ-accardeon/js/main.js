9b53be89a7ecb9d346dd1f112c671044bd8ba274
Ivan Volkov <volkov19@mail.ru>
2024-11-12 18:04:52 +0400

    To be continue...

--- Lesson-10/hw-10-JQ-accardeon/js/main.js
+++ Lesson-10/hw-10-JQ-accardeon/js/main.js
@@ -1,13 +1,20 @@
 $(function(){
   $('.ask').on('click', function(){
-  
-    if($(this).next().hasClass('open')){
-      $(this).next().slideUp(1000).removeClass('open');
-    }
-    else{
-      $('.answer').not($(this).next()).slideUp(1000).removeClass('open');
-      $(this).next().slideDown(1000).addClass('open');
-    } 
+    let answer = $(this).next();
+    $('.answer').not(answer).slideUp(1000);
+    answer.slideToggle(1000);
   })
 });
 
+// $(function(){
+//   $('.ask').on('click', function(){
+  
+//     if($(this).next().hasClass('open')){
+//       $(this).next().slideUp(1000).removeClass('open');
+//     }
+//     else{
+//       $('.answer').not($(this).next()).slideUp(1000).removeClass('open');
+//       $(this).next().slideDown(1000).addClass('open');
+//     } 
+//   })
+// });
